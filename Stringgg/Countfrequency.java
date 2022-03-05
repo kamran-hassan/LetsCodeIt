4983f675921a25a114b80c65be3cac0929bd7afc
KAMRAN HASSAN <hassan.kamran107@gmail.com>
2022-03-05 23:08:32 +0530

    spelling mistakes

--- Stringgg/Countfrequency.java
+++ Stringgg/Countfrequency.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Countfrequency {
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
-        System.out.println("plase input string");
+        System.out.println("please input string");
         String str = s.nextLine();
         long startTime = System.nanoTime();
         fastcountfrequency(str);
